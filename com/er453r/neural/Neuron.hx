9322bc68d7eabe01467f1d4ab1897978f2d8f405
Marcin Kotz <marcin.kotz@ives-system.com>
2017-01-11 15:30:30 +0100

    added basic types

--- com/er453r/neural/Neuron.hx
+++ com/er453r/neural/Neuron.hx
@@ -1,7 +1,16 @@
 package com.er453r.neural;
 
 class Neuron {
+	private var inputs:Array<Synapse> = [];
+	private var outputs:Array<Synapse> = [];
+
+	public var value:Float = 0;
+
 	public function new() {
-		trace("derp");
+		value = Math.random();
+	}
+
+	public function addInput(neuron:Neuron){
+		inputs.push(new Synapse(neuron, this));
 	}
 }
---
+++ com/er453r/neural/Synapse.hx
@@ -0,0 +1,15 @@
+package com.er453r.neural;
+
+class Synapse {
+	public var input:Neuron;
+	public var output:Neuron;
+
+	public var weight:Float;
+
+	public function new(input:Neuron, output:Neuron) {
+		this.input = input;
+		this.output = output;
+
+		this.weight = Math.random();
+	}
+}
