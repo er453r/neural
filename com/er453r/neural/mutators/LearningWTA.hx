57114322eb51567aacc8ba10c5242547fa696749
er453r <er453r@gmail.com>
2017-02-10 22:21:09 +0100

    logic failed approach

--- com/er453r/neural/mutators/LearningWTA.hx
+++ com/er453r/neural/mutators/LearningWTA.hx
@@ -3,6 +3,7 @@ package com.er453r.neural.mutators;
 class LearningWTA extends NeuronMutator{
 	private var treshold:Float;
 	private var momentum:Float = 0.9;
+	public var dl:Float = 0;
 
 	public function new(treshold:Float = 0.000001){
 		this.treshold = treshold;
@@ -11,31 +12,47 @@ class LearningWTA extends NeuronMutator{
 	override public function onStep(neuron:Neuron){
 		// weight modificaiton propagation
 		var max:Float = 0;
-		var maxInput:Synapse = neuron.inputs[0];
+		var maxSynapse:Synapse = neuron.inputs[0];
 
+		// we find the input with the biggest value
 		for(input in neuron.inputs){
 			var value:Float = input.getValue();
 
 			if(max < value){
 				max = value;
-				maxInput = input;
+				maxSynapse = input;
 			}
 		}
 
-		maxInput.dw = momentum * maxInput.dw + neuron.learning * neuron.value;
-		maxInput.weight += maxInput.dw;
-		maxInput.weight = maxInput.weight > 1 ? 1 : maxInput.weight;
+		// we increase the weighta when:
+		// the neuron is learning AND
+		// the neuron is active
+		maxSynapse.dw = momentum * maxSynapse.dw + neuron.learning * neuron.value;
+		maxSynapse.weight += maxSynapse.dw;
+		maxSynapse.weight = maxSynapse.weight > 1 ? 1 : maxSynapse.weight;
+		maxSynapse.weight *= 0.9998;
 
 		// learing signal propagation
 		max = 0;
+		maxSynapse = neuron.outputs[0];
 
+		// we find the output withe biggest learning factor
 		for(output in neuron.outputs){
 			var value:Float = output.output.learning * output.weight;
 
-			if(max < value)
+			if(max < value){
 				max = value;
+				maxSynapse = output;
+			}
 		}
 
-		neuron.learn = max;
+		// we are only learning if:
+		// we have acvity AND
+		// our output is learning
+		// (we do not check if output has activity,
+		// since we already check for out activity)
+		dl = momentum * dl + 0.1 * max;// * neuron.value;
+		neuron.learn = dl;
+		neuron.learn = neuron.learn > 1 ? 1 : neuron.learn;
 	}
 }
